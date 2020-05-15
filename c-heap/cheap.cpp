47efee91b32917244e18f589ef30fa17a730ed5f
jhpark93kr <65187582+jhpark93kr@users.noreply.github.com>
2020-05-14 23:53:46 -0700

    Update cheap.cpp

--- c-heap/cheap.cpp
+++ c-heap/cheap.cpp
@@ -1,11 +1,24 @@
-#include "cheap.h"
-
-void CHeap<int>::DestroyHeap();
+void CHeap<int>::DestroyHeap()
+{
+  DestroyList();
+}
 
 void CHeap<int>::Insert(const int& item);
+{
+  Insert(item);
+}
 
 void CHeap<int>::PeekTop(int& item) const;
+{
+  GetItem(item);
+}
 
 void CHeap<int>::Remove(int& item);
+{
+  Remove(item);
+}
 
 void CHeap<int>::RebuildHeap(int rootIndex);
+{
+  SetListSize(rootIndex);
+}
